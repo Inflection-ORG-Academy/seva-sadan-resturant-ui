9067884d3a936a885827b8b1d042b7d36c57a379
Deepak Maurya <deepak04122000maurya@gmail.com>
2026-01-09 08:28:27 +0530

    Scroll to top functionality aded

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, NavLink, Route, Routes } from "react-router";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Menus";
@@ -9,10 +9,29 @@ import OurStoryPage from "./pages/OurStory";
 import Hero from "./components/Hero";
 import CategoryDetails from "./pages/CategoryDetails";
 import Footer from "./components/Footer";
+import IntersectionObserverEx from "./pages/IntersectionObserverEx";
+import { ArrowUp } from "lucide-react";
 
 const App = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      console.log(window.scrollY);
+      if (window.scrollY > 400) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    });
+  }, []);
+
   return (
-    <>
+    <main className="bg-white">
       {/* <Navbar /> */}
       <Hero />
       <Routes>
@@ -21,10 +40,22 @@ const App = () => {
         <Route path="/our_story" element={<OurStoryPage />} />
         <Route path="/contact_us" element={<ContactUsPage />} />
         <Route path="/categories/:name" element={<CategoryDetails />} />
+        <Route
+          path="/intersection_observer"
+          element={<IntersectionObserverEx />}
+        />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
-    </>
+      <button
+        className={`fixed ${
+          isScrolled ? "z-10" : "-z-10"
+        } right-5 bottom-5 bg-white h-10 w-10 flex justify-center items-center rounded-full cursor-pointer`}
+        onClick={scrollToTop}
+      >
+        <ArrowUp />
+      </button>
+    </main>
   );
 };
 
